ac979fb0d9d11aedd300521f7b3df3ba21befa98
gki-webik <gki-webik@yandex.ru>
2024-06-05 18:57:51 +0600

    del

--- oatk/info-test/assets/script/index.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector('.header_btn');
-const header = document.querySelector('.header');
-btn.addEventListener('click', () => {
-    btn.classList.toggle('is-open');
-    header.classList.toggle('is-open');
-});
--- oatk/module-4-profi/10/js/index.js
+++
@@ -1,7 +0,0 @@
-const arr = ["Цитата N1", "Цитата N2", "Цитата N3", "Цитата N4", "Цитата N5", "Цитата N6", "Цитата N7", "Цитата N8", "Цитата N9", "Цитата N10"];
-setInterval(() => {
-    document.querySelector("blockquote").innerHTML = arr[Math.floor(Math.random() * arr.length)];
-}, 30000);
-document.querySelector(".btn").addEventListener("click", () => {
-    document.querySelector("blockquote").innerHTML = arr[Math.floor(Math.random() * arr.length)];
-});
